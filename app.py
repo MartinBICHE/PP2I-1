281e190585a46f4db486d51c4d38768b143e811d
Ethan Quilliot <ethan.quilliot@telecomnancy.eu>
2023-12-16 17:55:29 +0100

    Correction du quizz

--- app.py
+++ app.py
@@ -27,16 +27,21 @@ def quizz():
         ["Question 3", ["Réponse 3.1", "Réponse 3.2", "Réponse 3.3", "Réponse 3.4"], "Réponse 3.1"],
         ["Question 4", ["Réponse 4.1", "Réponse 4.2", "Réponse 4.3", "Réponse 4.4"], "Réponse 4.4"],
     ]
-    question_index = int(request.form.get('question_index',0))
+    try:
+        question_index = int(request.args.get('question_index'))
+    except TypeError:
+        question_index = 0
     current_question = questions[question_index] if question_index < len(questions) else None
     if request.method == 'POST':
-            user_response = request.form.get('reponse')
-            correct_answer = current_question[2]
-            is_correct = user_response == correct_answer
-            session['user_response'] = user_response 
-            session['is_correct'] = is_correct
-    return render_template('quizz.html',current_question=current_question, question_index=question_index,
-                           user_response=session.pop('user_response', None), is_correct=session.pop('is_correct',None))
+        user_response = request.form.get('reponse')
+        correct_answer = current_question[2]
+        print(user_response, correct_answer)
+        is_correct = user_response == correct_answer
+        return render_template('quizz.html', current_question=current_question,
+                               question_index=question_index, user_response=user_response,
+                               is_correct=is_correct)
+    else:
+        return render_template('quizz.html', current_question=current_question, question_index=question_index)
 
 
 @app.route('/resultats/')
