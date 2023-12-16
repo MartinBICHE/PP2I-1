7a83895def4638a37bd73c5876cc1efa3b6e5282
Leo <leo.gurtner@telecomnancy.eu>
2023-12-16 16:11:58 +0100

    'index'

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import *
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello'
+def index():
+    return render_template('index.html')
+
+@app.route('/register/')
+def register():
+    return 'Register'
+
+@app.route('/login/')
+def login():
+    return 'Login'
+
+@app.route('/quizz/')
+def quizz():
+    return 'Quizz'
+
+@app.route('/resultats/')
+def resultats():
+    return 'Resultats'
