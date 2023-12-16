3d3db80ec1f84b31917cd535f00516ec58b6bb29
WERCK Hugo <hugo.werck@telecomnancy.eu>
2023-12-16 15:28:24 +0100

    1ere version du quizz avec questions sur la meme page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,7 +15,13 @@ def login():
 
 @app.route('/quizz/')
 def quizz():
-    return 'Quizz'
+    questions = [
+        ["Question 1", ["Réponse 1.1", "Réponse 1.2", "Réponse 1.3", "Réponse 1.4"]],
+        ["Question 2", ["Réponse 2.1", "Réponse 2.2", "Réponse 2.3", "Réponse 2.4"]],
+        ["Question 3", ["Réponse 3.1", "Réponse 3.2", "Réponse 3.3", "Réponse 3.4"]],
+        ["Question 4", ["Réponse 4.1", "Réponse 4.2", "Réponse 4.3", "Réponse 4.4"]],
+    ]
+    return render_template('quizz.html',questions=questions)
 
 @app.route('/resultats/')
 def resultats():
