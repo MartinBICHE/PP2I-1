de30ec1ff92d639d85facf22567a38acfe3893ac
Ethan Quilliot <ethan.quilliot@telecomnancy.eu>
2023-12-16 14:32:26 +0100

    Squelette du fichier principal

--- app.py
+++ app.py
@@ -2,5 +2,21 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Index'
+
+@app.route('/register/')
+def register():
+    return 'Register'
+
+@app.route('/login/')
+def login():
+    return 'Login'
+
+@app.route('/quizz/')
+def quizz():
+    return 'Quizz'
+
+@app.route('/resultats/')
+def resultats():
+    return 'Resultats'
