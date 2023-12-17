cae06248ca9c1335e95bc21d8b3ec3844703049f
WERCK Hugo <hugo.werck@telecomnancy.eu>
2023-12-17 14:20:45 +0100

    Prise en compte de la base de données pour les questions et réponses du quizz

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
+
+
+def db_connection():
+    conn = sqlite3.connect('codesafe.db')
+    return conn
 
 
 @app.route('/')
@@ -20,33 +26,43 @@ def login():
 
 
 @app.route('/quizz/<categorie>', methods=['GET','POST'])
-def quizz(categorie):
-    questions = [
-        ["Question 1", ["Réponse 1.1", "Réponse 1.2", "Réponse 1.3", "Réponse 1.4"], "Réponse 1.2"],
-        ["Question 2", ["Réponse 2.1", "Réponse 2.2", "Réponse 2.3", "Réponse 2.4"], "Réponse 2.3"],
-        ["Question 3", ["Réponse 3.1", "Réponse 3.2", "Réponse 3.3", "Réponse 3.4"], "Réponse 3.1"],
-        ["Question 4", ["Réponse 4.1", "Réponse 4.2", "Réponse 4.3", "Réponse 4.4"], "Réponse 4.4"],
-    ]
-
+def quizz(categorie): 
     try:
         question_index = int(request.args.get('question_index'))
 
     except TypeError:
         question_index = 0
 
+    conn = db_connection()
+    cursor = conn.execute(f'SELECT question_txt FROM questions WHERE id_categorie={categorie}')
+    qs = cursor.fetchall()
+    questions = [item[0] for item in qs]
+  
     current_question = questions[question_index] if question_index < len(questions) else None
+
+    cursor1 = conn.execute(f'SELECT id_question FROM questions WHERE id_categorie={categorie}')
+    id = cursor1.fetchone()[0]
+
+    cursor2 = conn.execute(f'SELECT reponse_txt FROM reponses WHERE id_question={id}')
+    all_ans = cursor2.fetchall()
+    all_answers = [item[0] for item in all_ans]
+
+    cursor3 = conn.execute(f'SELECT reponse_txt FROM reponses WHERE id_question={id} AND is_good=1')
+    good = cursor3.fetchall()
+    correct_answer = [item[0] for item in good][0]
+    conn.close()
 
     if request.method == 'POST':
         user_response = request.form.get('reponse')
-        correct_answer = current_question[2]
         is_correct = user_response == correct_answer
         return render_template('quizz.html', current_question=current_question,
                                question_index=question_index, user_response=user_response,
-                               is_correct=is_correct, categorie=categorie)
-    
+                               is_correct=is_correct, categorie=categorie,
+                               all_answers=all_answers, correct_answer=correct_answer)
     else:
         return render_template('quizz.html', current_question=current_question, 
-                               question_index=question_index, categorie=categorie)
+                               question_index=question_index, categorie=categorie,
+                               all_answers=all_answers)
 
 
 @app.route('/resultats/')
