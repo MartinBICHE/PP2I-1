32f8cc558f7e0b03b773d0cf57c614fafd310860
WERCK Hugo <hugo.werck@telecomnancy.eu>
2023-12-17 11:19:19 +0100

    Prise en compte des categories de questions dans la route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -19,29 +19,34 @@ def login():
     return 'Login'
 
 
-@app.route('/quizz/', methods=['GET','POST'])
-def quizz():
+@app.route('/quizz/<categorie>', methods=['GET','POST'])
+def quizz(categorie):
     questions = [
         ["Question 1", ["Réponse 1.1", "Réponse 1.2", "Réponse 1.3", "Réponse 1.4"], "Réponse 1.2"],
         ["Question 2", ["Réponse 2.1", "Réponse 2.2", "Réponse 2.3", "Réponse 2.4"], "Réponse 2.3"],
         ["Question 3", ["Réponse 3.1", "Réponse 3.2", "Réponse 3.3", "Réponse 3.4"], "Réponse 3.1"],
         ["Question 4", ["Réponse 4.1", "Réponse 4.2", "Réponse 4.3", "Réponse 4.4"], "Réponse 4.4"],
     ]
+
     try:
         question_index = int(request.args.get('question_index'))
+
     except TypeError:
         question_index = 0
+
     current_question = questions[question_index] if question_index < len(questions) else None
+
     if request.method == 'POST':
         user_response = request.form.get('reponse')
         correct_answer = current_question[2]
-        print(user_response, correct_answer)
         is_correct = user_response == correct_answer
         return render_template('quizz.html', current_question=current_question,
                                question_index=question_index, user_response=user_response,
-                               is_correct=is_correct)
+                               is_correct=is_correct, categorie=categorie)
+    
     else:
-        return render_template('quizz.html', current_question=current_question, question_index=question_index)
+        return render_template('quizz.html', current_question=current_question, 
+                               question_index=question_index, categorie=categorie)
 
 
 @app.route('/resultats/')
